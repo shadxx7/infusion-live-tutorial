b860665846cb64eafe3a796179458f6ec03d8527
Mohamed Shadab <shadabdragon@gmail.com>
2018-03-17 02:38:34 +0530

    Huge changes; Layout revamp, added uiOptions etc

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,16 @@
+//Function for start
+function starter() {
+    document.getElementById("intro").style.display = "none";
+    document.getElementById("ex-comp").style.display = "block";
+    document.getElementById("ex1").style.display = "block";
+}
+
 // Function for 1st click
 function press1() {
     document.getElementById("ex1").style.display = "none";
-    document.getElementById('pre_').style.display = "none";
     document.getElementById("chall1").style.display = "block";
+    document.getElementById("chall_state").style.display = "block";
+    document.getElementById("get_hint").style.display = "block";
     document.getElementById('tut_pro').style.height = "25%";
 }
 
@@ -10,6 +18,8 @@ function press1() {
 function press2() {
     document.getElementById("ex-comp").style.display = "none";
     document.getElementById("chall1").style.display = "none";
+    document.getElementById("chall_state").style.display = "none";
+    document.getElementById("get_hint").style.display = "none";
     document.getElementById('ex-invoke').style.display = "block";
     document.getElementById("ex2").style.display = "block";
     document.getElementById('tut_pro').style.height = "50%";
@@ -18,15 +28,17 @@ function press2() {
 // Function for 3rd click
 function press3() {
     document.getElementById("ex2").style.display = "none";
+    document.getElementById("chall_state").style.display = "block";
+    document.getElementById("get_hint").style.display = "block";
     document.getElementById("chall2").style.display = "block";
     document.getElementById('tut_pro').style.height = "75%";
 }
 
 // Function for 4th click
 function press4() {
-    document.getElementById("ex-invoke").style.display = "none";
+    document.getElementById("chall_state").style.display = "none";
+    document.getElementById("get_hint").style.display = "none";
     document.getElementById("chall2").style.display = "none";
-    document.getElementById('intro').style.display = "none";
     document.getElementById('complete').style.display = "block";
     document.getElementById('tut_pro').style.height = "100%";
 }
